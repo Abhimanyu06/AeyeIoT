42be96826e240abe30391bcc2d5d008ea39bb987
Abhimanyu06 <62055457+Abhimanyu06@users.noreply.github.com>
2024-10-15 23:19:32 +0530

    update cases

--- aeye_run.sh
+++ aeye_run.sh
@@ -2,6 +2,7 @@
 sleep 15
 nohup python3 /home/rp/AeyeIoT/git_repo_version.py > /home/rp/AeyeIoT/logs/git_repo.log &
 chmod 777 /home/rp/AeyeIoT/* &
+chmod 777 /home/rp/AeyeIoT/logs/* &
 /home/rp/AeyeIoT/wifi-connect-headless-rpi/scripts/run.sh &
 nohup python3 /home/rp/AeyeIoT/resp_operations.py > /home/rp/AeyeIoT/logs/resp_operations.log &
 nohup python3 /home/rp/AeyeIoT/cv.py > /home/rp/AeyeIoT/logs/cv.log &
